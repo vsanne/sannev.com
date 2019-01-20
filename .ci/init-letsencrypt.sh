4ddb4f78f6426b8d229f4ac0f29bdefe8c1bcecc
Thilak Rao <rao@thilak.nl>
2019-01-20 17:24:12 +0100

    Fixed errors in nginx reverse proxy

--- .ci/init-letsencrypt.sh
+++ .ci/init-letsencrypt.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 domains=(thilak.nl www.thilak.nl)
 rsa_key_size=4096
-data_path="./certbot"
-email="nospam@thilak.nl" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+data_path="./data/certbot"
+email="npspam@thilak.nl"
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
